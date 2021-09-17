ec1454b32003568be016c09592f49b8a7347a885
Hridoy Mozumder <hrrarya6@gmail.com>
2021-09-17 23:35:03 +0600

    webpack setup for react

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = (env, argv) => {
+    var config = {
+        entry: './src/index.js',
+        output: {
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env', ['@babel/preset-react',{
+                                "pragma": "React.createElement",
+                                "pragmaFrag": "React.Fragment",
+                                "development": argv.mode === 'development'
+                            }]]
+                        }
+                    }
+                }
+            ]
+        }
+    };
+
+    return config;
+}
+
+
